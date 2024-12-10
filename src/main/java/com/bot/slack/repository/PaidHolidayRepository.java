855a0ace8a2b2949f1552719d476d224a55b4b5f
pochimoritaro <pochimoritaro@gmail.com>
2024-12-11 00:17:13 +0900

    承認ステータスをキーに検索できるクエリメソッドを追加

--- src/main/java/com/bot/slack/repository/PaidHolidayRepository.java
+++ src/main/java/com/bot/slack/repository/PaidHolidayRepository.java
@@ -1,9 +1,13 @@
 package com.bot.slack.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.bot.slack.entity.paidholiday.HolidayRquestStatus;
 import com.bot.slack.entity.paidholiday.PaidHoliday;
 
 @Repository
 public interface PaidHolidayRepository extends JpaRepository<PaidHoliday, Long> {
+    List<PaidHoliday> findByRequestStatus(HolidayRquestStatus requestStatus);
 }
