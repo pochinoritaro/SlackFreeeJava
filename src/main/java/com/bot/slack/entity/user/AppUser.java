b0ecade2c8c164bfd9206e39f9b9ace2bedbd159
pochimoritaro <pochimoritaro@gmail.com>
2024-12-10 23:04:31 +0900

    PaidHoliday用リレーションのテストを実装

--- src/main/java/com/bot/slack/entity/user/AppUser.java
+++ src/main/java/com/bot/slack/entity/user/AppUser.java
@@ -7,20 +7,25 @@ import lombok.Setter;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Column;
+import jakarta.persistence.CascadeType;
 
+import java.util.List;
 import java.util.UUID;
+
+import com.bot.slack.entity.paidholiday.PaidHoliday;
 
 
 @Entity
-@IdClass(value=AppUserId.class)
+@IdClass(value = AppUserId.class)
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AppUser {
     @Id
-    @Column(columnDefinition="UUID", nullable = false, updatable = false)
+    @Column(columnDefinition = "UUID", nullable = false, updatable = false)
     private UUID id = UUID.randomUUID();
 
     @Id
@@ -30,4 +35,7 @@ public class AppUser {
     private String employeeId;
 
     private String email;
+
+    @OneToMany(mappedBy = "appUser", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PaidHoliday> paidHoliday;
 }
