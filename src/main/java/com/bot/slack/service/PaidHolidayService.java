bac68cec46cb70d7a9a3af8542fae81c07b181eb
pochimoritaro <pochimoritaro@gmail.com>
2024-12-11 00:18:24 +0900

    findByRequestStatusを使用するgetAllHolidayRequestByStatusを実装

--- src/main/java/com/bot/slack/service/PaidHolidayService.java
+++ src/main/java/com/bot/slack/service/PaidHolidayService.java
@@ -1,8 +1,11 @@
 package com.bot.slack.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bot.slack.entity.paidholiday.HolidayRquestStatus;
 import com.bot.slack.entity.paidholiday.PaidHoliday;
 import com.bot.slack.repository.PaidHolidayRepository;
 
@@ -14,5 +17,9 @@ public class PaidHolidayService {
 
     public PaidHoliday createPaidHoliday(PaidHoliday paidHoliday) {
         return paidHolidayRepository.save(paidHoliday);
+    }
+
+    public List<PaidHoliday> getAllHolidayRequestByStatus(HolidayRquestStatus status) {
+        return paidHolidayRepository.findByRequestStatus(status);
     }
 }
